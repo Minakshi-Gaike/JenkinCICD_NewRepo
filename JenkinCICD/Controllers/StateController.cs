c505668c0ec8200555c8d03542080a82703eac95
Minakshi-Gaike <minakshigaike@gmail.com>
2026-06-25 17:50:29 +0530

    added new project

---
+++ JenkinCICD/Controllers/StateController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinCICD.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class StateController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/state")]
+        public IActionResult GetState()
+        {
+            return Ok("Application is running successfully.");
+        }
+    }
+}
